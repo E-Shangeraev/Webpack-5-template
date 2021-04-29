3f25e6efddd92415d29f87e9b05001bac49ed217
E-Shangeraev <e.shangeraev@gmail.com>
2021-04-29 23:26:11 +0700

    Настройка prettier. Первоначальная настройка eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,39 @@
-import * as $ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as $ from 'jquery'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import Post from '@models/Post';
-import './analytics';
-import xml from '@public/4764605_52.xml';
-import image from './assets/img/activities-4.jpg';
-import json from '@public/db';
+import Post from '@models/Post'
+import './analytics'
+import xml from '@public/4764605_52.xml'
+import json from '@public/db'
+import image from './assets/img/activities-4.jpg'
 
-import './styles/style.css';
-import './styles/scss.scss';
+import './styles/style.css'
+import './styles/scss.scss'
 
-console.log('Работает');
+console.log('Работает')
 
-const post = new Post('Webpack Post Title', image);
-$('pre').addClass('code').html(post.toString());
+const post = new Post('Webpack Post Title', image)
+$('pre').addClass('code').html(post.toString())
 
-const App = () => {
-  return (
-    <div className="container">
-      <h1>Hello Webpack</h1>
-      <hr />
-      <div className="logo"></div>
-      <hr />
-      <pre />
-      <div className="box">
-        <h2>SCSS</h2>
-      </div>
+const App = () => (
+  <div className="container">
+    <h1>Hello Webpack</h1>
+    <hr />
+    <div className="logo" />
+    <hr />
+    <pre />
+    <div className="box">
+      <h2>SCSS</h2>
     </div>
-  );
-};
+  </div>
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const array = ['apple', 'watermelon', 'strawberry']
+
+function a() {
+  const asd = 4324
+  const ASas = 13
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
